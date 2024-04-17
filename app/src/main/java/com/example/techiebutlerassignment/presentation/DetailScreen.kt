c65420e4e5339b7ba6d6c32c2bcf3dbd406f07eb
satheeeshkumarp <satkumar04@gmail.com>
2024-04-17 12:36:55 +0530

    bug fix

--- app/src/main/java/com/example/techiebutlerassignment/presentation/DetailScreen.kt
+++ app/src/main/java/com/example/techiebutlerassignment/presentation/DetailScreen.kt
@@ -1,18 +1,23 @@
 package com.example.techiebutlerassignment.presentation
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -30,9 +35,15 @@ fun DetailsScreen(jasonData: String) {
 
     Box(
         modifier = Modifier
-            .padding(16.dp)
+            .padding(0.dp)
     ) {
-        Column {
+        Column(modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = "Detailed view", fontSize = 20.sp,
+                fontWeight = FontWeight.Bold)
+
             Spacer(modifier = Modifier.height(16.dp))
             DetailProperty(label = "Id", value ="${data.id}" )
             DetailProperty(label = "UserId", value ="${data.userId}" )
